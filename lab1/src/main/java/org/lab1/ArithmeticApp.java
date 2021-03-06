441ab88d6dccf0446a3deffd2d372367652184a3
Rayan Daher <Rayan.daher811@gmail.com>
2021-03-06 20:50:53 +0200

    Adding new test file

--- lab1/src/main/java/org/lab1/ArithmeticApp.java
+++ lab1/src/main/java/org/lab1/ArithmeticApp.java
@@ -7,7 +7,7 @@ import static org.lab1.utilities.Constants.BASE_PATH_FOR_INPUT_FILES;
 public class ArithmeticApp {
     public static void main(String[] args){
         MathematicExpression expression =
-                IoFilesHandler.readExpressionInputFromFile(BASE_PATH_FOR_INPUT_FILES + "in1.txt");
+                IoFilesHandler.readExpressionInputFromFile(BASE_PATH_FOR_INPUT_FILES + "in5.txt");
         System.out.println(expression.getRawExpression());
 
         // Performing ouput file creation after calculation
