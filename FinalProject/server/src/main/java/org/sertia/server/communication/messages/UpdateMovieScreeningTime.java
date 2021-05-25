2aa7796a6b418d8a317d7aa61465109f676cd927
barakg <barakgonen9@gmail.com>
2021-05-25 23:37:00 +0300

    refactor msg name

--- FinalProject/server/src/main/java/org/sertia/server/communication/messages/UpdateMovieScreeningTime.java
+++ FinalProject/server/src/main/java/org/sertia/server/communication/messages/UpdateMovieScreeningTime.java
@@ -6,7 +6,7 @@ public class UpdateMovieScreeningTime {
     private BaseClientServerMessage metadata;
     private CinemaScreeningMovie currentMovie;
     private Date newDateTime;
-    private final String messageName = "UPDATE_MOVIE_SCREENING_TIME";
+    private final String messageName = "UPDATE_SCREENING_REQ";
 
     public UpdateMovieScreeningTime(String clientId, CinemaScreeningMovie currentMovie, Date newDateTime) {
         this.metadata = new BaseClientServerMessage(clientId);
